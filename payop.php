c9c27251f79fed094256f057201cdab5a8e34109
kuzzmich <kuzzmich828@gmail.com>
2022-01-19 11:22:17 +0200

    Update version

--- payop.php
+++ payop.php
@@ -4,7 +4,7 @@ Plugin Name: PayOp
 Plugin URI: https://wordpress.org/plugins/payop-woocommerce/
 Description: PayOp: Online payment processing service ➦ Accept payments online by 150+ methods from 170+ countries. Payments gateway for Growing Your Business in New Locations and fast online payments
 Author URI: https://payop.com/
-Version: 2.0.3
+Version: 2.0.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
