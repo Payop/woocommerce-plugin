59757fd23622a6d3d38773a85bbbb55b29c47a89
Edward Z <eduard.z@payop.com>
2022-05-09 08:25:14 +0200

    Logger include

--- Logger.php
+++ Logger.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Payop;
-
 /**
  * Class Logger
  * @package Payop
--- payop.php
+++ payop.php
@@ -10,6 +10,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
 */
 
+require_once 'Logger.php';
 
 if(!defined('ABSPATH')){
     exit;
